5c87d37a5fb4fa0917c2df7b7ef04e97dc9f0d6d
alissonfgc <alissonfgc01@gmail.com>
2024-07-06 06:42:51 -0300

    Initial commit

---
+++ src/main/java/com/alissonfgc/jpacourse/JpacourseApplication.java
@@ -0,0 +1,13 @@
+package com.alissonfgc.jpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissonfgc/jpacourse/JpacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonfgc.jpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
