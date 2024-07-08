6e8347733e5b0e7223dc439f6dc5245db3cbf247
alissonfgc <alissonfgc01@gmail.com>
2024-07-08 07:49:53 -0300

    Subtotal & Total methods

--- src/main/java/com/alissonfgc/jpacourse/entities/Order.java
+++ src/main/java/com/alissonfgc/jpacourse/entities/Order.java
@@ -39,7 +39,7 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
@@ -97,6 +97,14 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+	
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem oi : items) {
+			sum += oi.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
