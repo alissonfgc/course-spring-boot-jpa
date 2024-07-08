e42adfa41eba40af33b4fc439fdec0f3960a7f0d
alissonfgc <alissonfgc01@gmail.com>
2024-07-08 02:11:27 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/alissonfgc/jpacourse/entities/User.java
+++ src/main/java/com/alissonfgc/jpacourse/entities/User.java
@@ -1,12 +1,17 @@
 package com.alissonfgc.jpacourse.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -72,6 +81,14 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders(){
+		return this.orders;
+	}
+	
+	public void addOrder(Order order){
+		this.orders.add(order);
 	}
 
 	@Override
