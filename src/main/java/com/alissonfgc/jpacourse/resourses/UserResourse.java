371cd1e992416260d0a740d3fa465e4fddc9174b
alissonfgc <alissonfgc01@gmail.com>
2024-07-07 23:57:25 -0300

    User entity and resource

---
+++ src/main/java/com/alissonfgc/jpacourse/resourses/UserResourse.java
@@ -0,0 +1,20 @@
+package com.alissonfgc.jpacourse.resourses;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alissonfgc.jpacourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResourse {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
