486434039b292196bfbebcb332e5a0c3fefebcc8
alissonfgc <alissonfgc01@gmail.com>
2024-07-09 15:24:59 -0300

    Exception handling - findById

--- src/main/java/com/alissonfgc/jpacourse/repositories/OrderItemRepository.java
+++ src/main/java/com/alissonfgc/jpacourse/repositories/OrderItemRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alissonfgc.jpacourse.entities.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-	
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
 }
